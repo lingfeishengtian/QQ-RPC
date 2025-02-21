df70db92c88270f09556074874b1b75030d70e46
Hunter Han <hunter.han@gmail.com>
2025-02-21 13:24:57 -0600

    Initial Commit

---
+++ QQ音乐RPC/QQ__RPCApp.swift
@@ -0,0 +1,17 @@
+//
+//  QQ__RPCApp.swift
+//  QQ音乐RPC
+//
+//  Created by Hunter Han on 2/21/25.
+//
+
+import SwiftUI
+
+@main
+struct QQ__RPCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ QQ音乐RPCTests/QQ__RPCTests.swift
@@ -0,0 +1,17 @@
+//
+//  QQ__RPCTests.swift
+//  QQ音乐RPCTests
+//
+//  Created by Hunter Han on 2/21/25.
+//
+
+import Testing
+@testable import QQ__RPC
+
+struct QQ__RPCTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
